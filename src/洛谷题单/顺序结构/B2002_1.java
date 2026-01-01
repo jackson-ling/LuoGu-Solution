afd415ec65dbae26ca13c444e1e607c914c9b4a2
jackson-ling <1524205121@qq.com>
2026-01-02 01:24:37 +0800

    update 2026.01.02

---
+++ src/洛谷题单/顺序结构/B2002_1.java
@@ -0,0 +1,16 @@
+package 洛谷题单.顺序结构;
+
+/**
+ * ClassName: B2002_1
+ * Package: 洛谷题单.顺序结构
+ * Description:
+ *
+ * @author jacksonling
+ * @version 1.0
+ * @Date 2026-01-02 1:17
+ */
+public class B2002_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
